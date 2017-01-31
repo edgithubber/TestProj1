07155dfea11c8677ae38463f6cd8df16acddb245
Ed Pondoc <edpondoc@MyMacVxx.local>
2017-02-01 03:47:38 +0800

    Initial Commit

---
+++ TestProj1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestProj1
+//
+//  Created by Ed Pondoc on 1/2/17.
+//  Copyright © 2017 Ed Pondoc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
